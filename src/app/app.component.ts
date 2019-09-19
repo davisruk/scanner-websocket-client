dde53c335af6bf30dc3188e13ac85ddad8a81058
Richard Davis <rich@davisfamily.eu>
2019-09-19 13:19:29 +0100

    Initial Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { WebSocketAPI } from './web-socket-api';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'scanner-websocker-client';
+export class AppComponent implements OnInit {
+  constructor(private ref: ChangeDetectorRef) {}
+
+  title = 'Scanner WebSocket Client';
+
+  webSocketAPI: WebSocketAPI;
+  greeting: string;
+  name: string;
+  connected: boolean;
+
+  ngOnInit() {
+    this.webSocketAPI = new WebSocketAPI(this);
+  }
+
+  connect() {
+    this.webSocketAPI._connect();
+    this.connected = true;
+  }
+
+  disconnect() {
+    this.webSocketAPI._disconnect();
+    this.connected = false;
+  }
+
+  handleMessage(message: string) {
+    this.greeting = message;
+  }
 }
---
+++ src/app/web-socket-api.ts
@@ -0,0 +1,49 @@
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { AppComponent } from './app.component';
+
+export class WebSocketAPI {
+  webSocketEndPoint = 'http://localhost:8080/ws';
+  topic = '/topic/greetings';
+  stompClient: any;
+  appComponent: AppComponent;
+
+  constructor(appComponent: AppComponent) {
+    this.appComponent = appComponent;
+  }
+
+  _connect() {
+    console.log('Initialize WebSocket Connection');
+    const ws = new SockJS(this.webSocketEndPoint);
+    this.stompClient = Stomp.over(ws);
+    this.stompClient.connect(
+      {},
+      frame => {
+        this.stompClient.subscribe(this.topic, sdkEvent => {
+          this.onMessageReceived(sdkEvent);
+        });
+      },
+      this.errorCallBack
+    );
+  }
+
+  _disconnect() {
+    if (this.stompClient !== null) {
+      this.stompClient.disconnect();
+    }
+    console.log('Disconnected');
+  }
+
+  // on error, schedule a reconnection attempt
+  errorCallBack(error) {
+    console.log('errorCallBack -> ' + error);
+    setTimeout(() => {
+      this._connect();
+    }, 5000);
+  }
+
+  onMessageReceived(message) {
+    console.log('Message Recieved from Server :: ' + message);
+    this.appComponent.handleMessage(message.body);
+  }
+}
